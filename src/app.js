ea7ef33d425e6465efe153827a26de8592d67cd7
neoglez <yansel.gonzalez-tejeda@cybly.tech>
2022-10-04 18:09:21 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import { createSSRApp } from 'vue';
 import { vueComponent as App } from './components/App.js';
-import { h } from 'vue';
 
 export function createApp() {
-  return createSSRApp({
-    render: () => h(App),
-  });
+  return createSSRApp(App);
 }
--- src/server.js
+++ src/server.js
@@ -1,11 +1,60 @@
 import { createApp } from './app.js';
 import { renderToString } from 'vue/server-renderer';
 import * as Http from 'http';
-import * as Path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as url from 'url';
 
 const server = Http.createServer()
   .on('request', async (req, res) => {
     let html = '';
+      // parse URL
+  const parsedUrl = url.parse(req.url);
+  // extract URL path
+  let pathname = `.${parsedUrl.pathname}`;
+  // based on the URL path, extract the file extension. e.g. .js, .doc, ...
+  const ext = path.parse(pathname).ext ? path.parse(pathname).ext : '';
+  // maps file extension to MIME typere
+  const map = {
+    '.ico': 'image/x-icon',
+    '.html': 'text/html',
+    '.js': 'text/javascript',
+    '.json': 'application/json',
+    '.css': 'text/css',
+    '.png': 'image/png',
+    '.jpg': 'image/jpeg',
+    '.wav': 'audio/wav',
+    '.mp3': 'audio/mpeg',
+    '.svg': 'image/svg+xml',
+    '.pdf': 'application/pdf',
+    '.doc': 'application/msword'
+  };
+  console.log(pathname);
+
+    fs.access(pathname, function (exist) {
+      if(!exist) {
+        // if the file is not found, return 404
+        res.statusCode = 404;
+        res.end(`File ${pathname} not found!`);
+        return;
+      }
+
+      // if is a directory search for index file matching the extension
+      //if (fs.statSync(pathname).isDirectory()) pathname += '/index' + ext;
+
+      // read file from file system
+      fs.readFile(pathname, function(err, data){
+        if(err){
+          res.statusCode = 500;
+          res.end(`Error getting the file: ${err}. Pathname was ${pathname}.` );
+        } else {
+          // if the file is found, set Content-type and send data
+          res.setHeader('Content-type', map[ext] || 'text/plain' );
+          res.end(data);
+        }
+      });
+    });
+
     const app = createApp();
     try {
       html = await renderToString(app);
@@ -16,7 +65,9 @@ const server = Http.createServer()
       html = 'Error';
       console.error('Failed SSR', e);
     }
-    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    if (req.url == 'css/inde.')
+
     res.end(`
     <!DOCTYPE html>
     <html lang="en">
@@ -45,6 +96,6 @@ const server = Http.createServer()
     </html>
     `);
   })
-  .listen(3000, null, () => {
+  .listen(3000, undefined, () => {
     console.log(`Server running on ${JSON.stringify(server.address())}`);
   });
